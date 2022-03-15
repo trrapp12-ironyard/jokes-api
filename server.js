901c10584e3721266d5958e6b31a9e66c5009fe6
Trevor Rapp <trrapp@gmail.com>
2022-03-14 23:21:12 -0600

    fix port issue

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js 
 // taken from https://www.npmjs.com/package/json-server documentation
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 // server.listen(3000, () => {
 //   console.log('JSON Server is running')
 // })
